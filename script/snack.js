1fe26fd32c32477939c4c0f3a716a8ba6f055036
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-03-18 21:14:27 +0100

    first commit

---
+++ script/snack.js
@@ -0,0 +1,32 @@
+
+
+/////// SNACK 1 
+
+// Crea una funzione che somma due numeri.
+// Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma.
+// Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+// Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+
+somma(100, 200);
+
+function somma(n1, n2) {
+    const sum = n1 + n2;
+    console.log(`funzione dichiarativa, somma di ${n1} + ${n2} = ${sum}`);
+}
+
+
+
+
+const sumAnonima = function (n1, n2) {
+    const sum = n1 + n2;
+    console.log(`funzione anonima, somma di ${n1} + ${n2} = ${sum}`);
+
+}
+
+sumAnonima(400, 300)
+
+const sumArrow = (n1, n2) => {
+    const sum = n1 + n2; console.log(`funzione con sintassi arrow function, somma di ${n1} + ${n2} = ${sum}`);
+}
+
+sumArrow(40, 40)
