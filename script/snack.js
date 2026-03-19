ed1872a773b68f23d9d5e6bc482f39952cb871a7
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-03-19 10:55:25 +0100

    snack 7 done

--- script/snack.js
+++ script/snack.js
@@ -145,3 +145,27 @@ function creaContatoreAutomatico(ms) {
 
 
 creaContatoreAutomatico(1000)
+
+
+
+////// SNACK 7
+
+// Crea una funzione che ferma un timer dopo un certo tempo
+// Scrivi una funzione eseguiEferma che accetta un messaggio,
+// un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari,
+// ma si deve fermare dopo il tempo di stop.
+
+
+
+function eseguiEferma(message, start, end) {
+    const interval = setInterval(() => {
+        console.log(message);
+
+    }, start)
+    setInterval(() => {
+        clearInterval(interval)
+    }, end)
+}
+
+
+eseguiEferma("ciao", 1000, 5000);
